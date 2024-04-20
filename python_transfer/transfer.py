26ca2c4b7cc76bfcaef30b1f01642e6bd77f0ec3
Panurat <paunavt5@gmail.com>
2024-04-20 12:54:50 +0700

    Reading code html and to process

--- python_transfer/transfer.py
+++ python_transfer/transfer.py
@@ -1,7 +1,7 @@
 from lxml import html
 
 # อ่านไฟล์ MHTML
-with open('test.mhtml', 'rb') as f:
+with open('test.mhtml', 'r', encoding='utf-8') as f:
     mhtml_content = f.read()
 
 # แปลง MHTML เป็น HTML
@@ -9,7 +9,7 @@ tree = html.fromstring(mhtml_content)
 html_content = tree.xpath('//body')[0].text_content()
 
 # บันทึกไฟล์ HTML
-with open('converted_file.html', 'w', encoding='utf-8') as f:
+with open('test-converted.php', 'w', encoding='utf-8') as f:
     f.write(html_content)
 
-print('File converted successfully!')
+print('ไฟล์ HTML ถูกสร้างเรียบร้อยแล้ว!')
